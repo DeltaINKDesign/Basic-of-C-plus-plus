1ef06da676551903a0e4fcfafb2ad58da23f69fb
Delta Ink Design <izula97@gmail.com>
2019-01-15 15:54:47 +0100

    first comment

---
+++ Christmas tree/Christmas tree.cpp
@@ -0,0 +1,215 @@
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+
+ /*  
+ Drugi spos鏏 
+system("COLOR 1B");   pierwszy znak t這 druki czcionka;
+0 - czarny
+1 - niebieski
+2 - zielony
+3 - b喚kitny
+4 - czerwony
+5 - purpurowy
+6 - 鄴速y
+7 - bia造
+8 - szary
+9 - jasnoniebieski
+A - jasnozielony
+B - jasnob喚kitny
+C - jasnoczerwony
+D - jasnopurpurowy
+E - jasno鄴速y
+F - jaskrawobia造
+*/ 
+
+HANDLE hOut;
+    hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+
+
+#define C4  261
+
+#define D4  293
+
+#define E4  329
+#define F4  349
+
+#define G4  392
+
+#define A4  440
+
+#define B4  493
+ 
+#define C5  523
+
+#define D5  587
+
+#define E5  659
+#define F5  698
+#define G5  783
+
+#define A5  880
+
+#define B5  987
+  
+#define full 600
+#define half full 
+
+
+
+
+   for(;;)
+   {
+   	
+   	
+   	system("COLOR 02");
+   cout<<"                ^               			 "<<endl;
+   	
+   cout<<"               / \\            			  "<<endl;
+
+   cout<<"              *   *            			  "<<endl;
+ 
+   cout<<"             /     \\         			   "<<endl;
+   cout<<"            * * * * *       				  "<<endl;
+   cout<<"           * * * * * *       					 "<<endl;
+   cout<<"          /           \\     				    "<<endl;
+   cout<<"         * * * * * * * *    					  "<<endl;
+   cout<<"        * * * * * * * * *    					   "<<endl;
+   cout<<"       /                 \\  					  "<<endl;
+   cout<<"       O     O     O    O      				 "<<endl;
+   cout<<"     /                     \\  				  "<<endl;
+   cout<<"     * * * * * * * * * * * *    				 "<<endl;
+   cout<<"    * * * * * * * * * * * * *   				 "<<endl;
+   cout<<"   /                         \\               "<<endl;
+   cout<<"   ________________________               "<<endl;
+   cout<<"              |   |                           "<<endl;
+   cout<<"        .................                     "<<endl;
+   cout<<"                                              "<<endl;
+    cout<< "   ------ -------- ----- ------";
+   Sleep(100);
+   system( "cls");
+   
+   HANDLE hOut;
+    hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    
+   
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"                ^               			 "<< flush <<endl;
+   cout<<"               / \\            			  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 06 );
+   cout<<"              *   *            			  "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"             /     \\         			   "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 04 );
+   cout<<"            * * * * *       				  "<< flush <<endl;
+   cout<<"           * * * * * *       					 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"          /           \\     				    "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 06 );
+   cout<<"         * * * * * * * *    					  "<< flush <<endl;
+   cout<<"        * * * * * * * * *    					   "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"       /                 \\  					  "<< flush <<endl;
+   cout<<"       O     O     O    O      				 "<< flush <<endl;
+   cout<<"     /                     \\  				  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 04 );
+   cout<<"     * * * * * * * * * * * *    				 "<< flush <<endl;
+   cout<<"    * * * * * * * * * * * * *   				 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"   /                         \\               "<< flush <<endl;
+   cout<<"  O____________________________O              "<< flush <<endl;
+   cout<<"              |   |                           "<< flush <<endl;
+   cout<<"        .................                     "<< flush <<endl;
+   cout<<"                                              "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 06 );
+   cout<< "   ------ WESOLYCH SWIAT ------";
+   Sleep(300);
+
+  
+
+    
+    
+     system( "cls" );
+  
+    
+   
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"                ^               			 "<< flush <<endl;
+   cout<<"               / \\            			  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 05 );
+   cout<<"              *   *            			  "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"             /     \\         			   "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 03 );
+   cout<<"            * * * * *       				  "<< flush <<endl;
+   cout<<"           * * * * * *       					 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"          /           \\     				    "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 05 );
+   cout<<"         * * * * * * * *    					  "<< flush <<endl;
+   cout<<"        * * * * * * * * *    					   "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"       /                 \\  					  "<< flush <<endl;
+   cout<<"       O     O     O    O      				 "<< flush <<endl;
+   cout<<"     /                     \\  				  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 03 );
+   cout<<"     * * * * * * * * * * * *    				 "<< flush <<endl;
+   cout<<"    * * * * * * * * * * * * *   				 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"   /                         \\               "<< flush <<endl;
+   cout<<"  O____________________________O              "<< flush <<endl;
+   cout<<"              |   |                           "<< flush <<endl;
+   cout<<"        .................                     "<< flush <<endl;
+   cout<<"                                              "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 05 );
+   cout<< "   ------ WESOLYCH SWIAT ------";
+   Sleep(300);
+
+   
+   system( "cls" );
+   
+   SetConsoleTextAttribute( hOut, 02);
+   cout<<"                ^               			 "<< flush <<endl;
+   cout<<"               / \\            			  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 04 );
+   cout<<"              *   *            			  "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"             /     \\         			   "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 05 );
+   cout<<"            * * * * *       				  "<< flush <<endl;
+   cout<<"           * * * * * *       					 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"          /           \\     				    "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 03 );
+   cout<<"         * * * * * * * *    					  "<< flush <<endl;
+   cout<<"        * * * * * * * * *    					   "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"       /                 \\  					  "<< flush <<endl;
+   cout<<"       O     O     O    O      				 "<< flush <<endl;
+   cout<<"     /                     \\  				  "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 04 );
+   cout<<"     * * * * * * * * * * * *    				 "<< flush <<endl;
+   cout<<"    * * * * * * * * * * * * *   				 "<< flush <<endl;
+    SetConsoleTextAttribute( hOut, 02);
+   cout<<"   /                         \\               "<< flush <<endl;
+   cout<<"  O____________________________O              "<< flush <<endl;
+   cout<<"              |   |                           "<< flush <<endl;
+   cout<<"        .................                     "<< flush <<endl;
+   cout<<"                                              "<< flush <<endl;
+   SetConsoleTextAttribute( hOut, 03 );
+   cout<< "   ------ WESOLYCH SWIAT ------";
+   Sleep(300);
+
+   
+   system( "cls" );
+  
+  
+}
+   
+    return 0;
+}
